af7ba566f1479d360f5e610f0753f18275bba83b
czeros003 <czeros003@gmail.com>
2022-04-26 00:25:40 +0200

    README.md update, new src directory

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # CrunchEngine
 Diploma by Wojciech Czerski
 
-important -> imgui linking files are broken, need to specify "imgui.h" to "../imghui.h"
-####git clone --recursive https://github.com/ocornut/imgui.git -b docking
+---
+- ### IMGUI
+`git clone --recursive https://github.com/ocornut/imgui.git -b docking`  
+important -> imgui linking files are broken, need to specify "imgui.h" to "../imghui.h" in CMAKE
+
+---
+- ### GLFW
+`git clone https://github.com/glfw/glfw`
+
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 // If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
-#include <IMGUI/imgui.h>
-#include <IMGUI/backends/imgui_impl_glfw.h>
-#include <IMGUI/backends/imgui_impl_opengl3.h>
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 #include <stdio.h>
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include "glfw/include/GLFW/glfw3.h" // Will drag system OpenGL headers
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
