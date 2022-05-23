7179f4634aed1b46ac4791a85a412b9fc8c25f88
czeros003 <czeros003@gmail.com>
2022-05-24 00:04:13 +0200

    adding RET as submodule fix

---
+++ cmake-build-default/CMakeFiles/CrunchEngine.dir/appicon.i
@@ -0,0 +1,2 @@
+#line 1 "D:\\Wojtek-uczelnia\\EPG\\CrunchEngine\\appicon.rc"
+IDI_ICON1 ICON DISCARDABLE "appicon.ico"
---
+++ cmake-build-default/libs/CMakeFiles/IMGUI.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/backends/imgui_impl_glfw.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/backends/imgui_impl_glfw.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/backends/imgui_impl_opengl3.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/backends/imgui_impl_opengl3.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/imgui.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/imgui.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/imgui.cpp.obj.d"
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/imgui_demo.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_demo.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_demo.cpp.obj.d"
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/imgui_draw.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_draw.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_draw.cpp.obj.d"
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/imgui_tables.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_tables.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_tables.cpp.obj.d"
+  "D:/Wojtek-uczelnia/EPG/CrunchEngine/libs/imgui/imgui_widgets.cpp" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_widgets.cpp.obj" "gcc" "libs/CMakeFiles/IMGUI.dir/imgui/imgui_widgets.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
